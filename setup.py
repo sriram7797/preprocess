4cce9d05e4bd7c541a4128c0c4a09bd7419998d2
Sriram <sriram.appusamy@exterro.com>
2020-01-20 12:39:55 +0530

    s

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="preprocess-sriram7797", # Replace with your own username
+    name="sriram", # Replace with your own username
     version="0.0.1",
     author="Sriram",
     author_email="shreeram7797@gmail.com",
